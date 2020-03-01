5a2c06b104be20469e1c64afdfbee70b1048257f
Stela Capsa <stela.capsa@gmail.com>
2020-03-01 18:38:52 +0000

    Automatically backed up by Learn

---
+++ db/migrate/005_create_costumes.rb
@@ -0,0 +1,11 @@
+class CreateCostumes < ActiveRecord::Migration[5.0]
+  def change 
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
